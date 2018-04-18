1cad31b600af13fe1d999b59f3884f08866472cf
Macro303 <jackson.jonah@gmail.com>
2018-04-18 22:39:57 +1200

    Add Console Dependency. Add Menu

---
+++ src/main/java/macro303/neptunes_pride/Config.java
@@ -0,0 +1,123 @@
+package macro303.neptunes_pride;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+class Config {
+	private transient static final Gson gson = new GsonBuilder()
+			.serializeNulls()
+			.setPrettyPrinting()
+			.disableHtmlEscaping()
+			.create();
+	private transient static final File configFile = new File("bin", "config.json");
+
+	private long gameID;
+	@NotNull
+	private HashMap<String, String> players = new HashMap<>();
+	@Nullable
+	private String proxyHostname = null;
+	private int proxyPort = -1;
+
+	Config(long gameID) {
+		this.gameID = gameID;
+	}
+
+	static Config loadConfig() {
+		if (!configFile.getParentFile().exists())
+			configFile.getParentFile().mkdirs();
+		Config config = null;
+		try (FileReader fr = new FileReader(configFile)) {
+			config = gson.fromJson(fr, Config.class);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			if (config == null) {
+				saveConfig(new Config(1));
+				config = loadConfig();
+			}
+		}
+		return config;
+	}
+
+	static void saveConfig(@NotNull Config config) {
+		try (FileWriter fw = new FileWriter(configFile)) {
+			gson.toJson(config, fw);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	long getGameID() {
+		return gameID;
+	}
+
+	void setGameID(long gameID) {
+		this.gameID = gameID;
+	}
+
+	@NotNull
+	HashMap<String, String> getPlayers() {
+		return players;
+	}
+
+	void setPlayers(@NotNull HashMap<String, String> players) {
+		this.players = players;
+	}
+
+	@Nullable
+	String getProxyHostname() {
+		return proxyHostname;
+	}
+
+	void setProxyHostname(@Nullable String proxyHostname) {
+		this.proxyHostname = proxyHostname;
+	}
+
+	int getProxyPort() {
+		return proxyPort;
+	}
+
+	void setProxyPort(int proxyPort) {
+		this.proxyPort = proxyPort;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Config)) return false;
+
+		Config config = (Config) o;
+
+		if (gameID != config.gameID) return false;
+		if (proxyPort != config.proxyPort) return false;
+		if (!players.equals(config.players)) return false;
+		return proxyHostname != null ? proxyHostname.equals(config.proxyHostname) : config.proxyHostname == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = (int) (gameID ^ (gameID >>> 32));
+		result = 31 * result + players.hashCode();
+		result = 31 * result + (proxyHostname != null ? proxyHostname.hashCode() : 0);
+		result = 31 * result + proxyPort;
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "Config{" +
+				"gameID=" + gameID +
+				", players=" + players +
+				", proxyHostname='" + proxyHostname + '\'' +
+				", proxyPort=" + proxyPort +
+				'}';
+	}
+}
--- src/main/java/macro303/neptunes_pride/NeptunesPride.java
+++ src/main/java/macro303/neptunes_pride/NeptunesPride.java
@@ -2,52 +2,115 @@ package macro303.neptunes_pride;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import macro303.console.Console;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.*;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.HashMap;
 import java.util.TreeSet;
 
 /**
  * Created by Macro303 on 2018-04-17.
  */
 class NeptunesPride {
-	private final static String apiAddress = "http://nptriton.cqproject.net/game/";
-	private final static long gameNumber = 4620725739847680L;
+	private static final String apiAddress = "http://nptriton.cqproject.net/game/";
+	private Config config;
 
 	private NeptunesPride() {
-		TreeSet<Player> allPlayers = getAllPlayers();
-		allPlayers.forEach(this::showPlayer);
+		loadConfig();
+		mainMenu();
 	}
 
 	public static void main(@NotNull String... args) {
 		new NeptunesPride();
 	}
 
-	private void showPlayer(@NotNull Player player) {
-		StringBuilder builder = new StringBuilder();
-		for (int i = 0; i < player.getAlias().length() + 4; i++) {
-			builder.append("=");
+	private void loadConfig() {
+		config = Config.loadConfig();
+		if (config == null) {
+			Config.saveConfig(new Config(-1L));
+			config = Config.loadConfig();
 		}
-		System.out.println(builder.toString());
-		System.out.println("  " + player.getAlias() + "  ");
-		System.out.println(builder.toString());
-		System.out.println("Strength: " + player.getTotalStrength());
-		System.out.println("Stars: " + player.getTotalStars());
-		System.out.println("Fleets: " + player.getTotalFleets());
-		System.out.println("Total Stats: " + player.getTotalStats());
-		System.out.println("Technology:");
-		System.out.println("\tScanning: " + player.getPlayerTechnology().getScanning().getLevel());
-		System.out.println("\tPropulsion: " + player.getPlayerTechnology().getPropulsion().getLevel());
-		System.out.println("\tTerraforming: " + player.getPlayerTechnology().getTerraforming().getLevel());
-		System.out.println("\tResearch: " + player.getPlayerTechnology().getResearch().getLevel());
-		System.out.println("\tWeapons: " + player.getPlayerTechnology().getWeapons().getLevel());
-		System.out.println("\tBanking: " + player.getPlayerTechnology().getBanking().getLevel());
-		System.out.println("\tManufacturing: " + player.getPlayerTechnology().getManufacturing().getLevel());
+	}
+
+	private void mainMenu() {
+		int option;
+		do {
+			option = Console.displayMenu(new String[]{"All Players", "Player By Alias", "Player By Name", "Exit"}, "Main Menu");
+			switch (option) {
+				case 1:
+					TreeSet<Player> allPlayers = getAllPlayers();
+					allPlayers.forEach(this::showPlayer);
+					break;
+				case 2:
+					Player aliasPlayer = getPlayerByAlias(Console.displayPrompt("Alias"));
+					showPlayer(aliasPlayer);
+					break;
+				case 3:
+					Player namePlayer = getPlayerByName(Console.displayPrompt("Name"));
+					showPlayer(namePlayer);
+					break;
+			}
+		} while (option != 0);
+	}
+
+	@Nullable
+	private Player getPlayerByAlias(@Nullable String alias) {
+		return getAllPlayers().stream().filter(player -> player.getAlias().equalsIgnoreCase(alias)).findFirst().orElse(null);
+	}
+
+	@Nullable
+	private Player getPlayerByName(@Nullable String name) {
+		return getPlayerByAlias(config.getPlayers().get(name));
+	}
+
+	private void showPlayer(@Nullable Player player) {
+		if(player == null){
+			Console.displayWarning("No Player Found");
+		}else {
+			Console.displayHeading(player.getAlias());
+			Console.displayItemValue("Strength", player.getTotalStrength());
+			Console.displayItemValue("Stars", player.getTotalStars());
+			Console.displayItemValue("Fleets", player.getTotalFleets());
+			Console.displayItemValue("Total Stats", player.getTotalStats());
+			Console.displayMessage("Technology:");
+			Console.displayItemValue("\tScanning", player.getPlayerTechnology().getScanning().getLevel());
+			Console.displayItemValue("\tPropulsion", player.getPlayerTechnology().getPropulsion().getLevel());
+			Console.displayItemValue("\tTerraforming", player.getPlayerTechnology().getTerraforming().getLevel());
+			Console.displayItemValue("\tResearch", player.getPlayerTechnology().getResearch().getLevel());
+			Console.displayItemValue("\tWeapons", player.getPlayerTechnology().getWeapons().getLevel());
+			Console.displayItemValue("\tBanking", player.getPlayerTechnology().getBanking().getLevel());
+			Console.displayItemValue("\tManufacturing", player.getPlayerTechnology().getManufacturing().getLevel());
+		}
+	}
+
+	private String getApiAddress() {
+		return apiAddress + config.getGameID() + "/";
+	}
+
+	private HttpURLConnection getConnection(@NotNull String address) throws IOException {
+		HttpURLConnection connection;
+		URL url = new URL(getApiAddress() + address);
+		if (config.getProxyHostname() != null && config.getProxyPort() != -1) {
+			Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(config.getProxyHostname(), config.getProxyPort()));
+			connection = (HttpURLConnection) url.openConnection(proxy);
+		} else {
+			connection = (HttpURLConnection) url.openConnection();
+		}
+		connection.setRequestMethod("GET");
+		connection.connect();
+		return connection;
 	}
 
 	@NotNull
@@ -55,17 +118,7 @@ class NeptunesPride {
 		TreeSet<Player> players = new TreeSet<>();
 		HttpURLConnection connection = null;
 		try {
-			URL url = new URL(apiAddress + gameNumber + "/players");
-			try {
-				Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("bit.datacom.co.nz", 3128));
-				connection = (HttpURLConnection) url.openConnection(proxy);
-				connection.setRequestMethod("GET");
-				connection.connect();
-			}catch (UnknownHostException uhe){
-				connection = (HttpURLConnection) url.openConnection();
-				connection.setRequestMethod("GET");
-				connection.connect();
-			}
+			connection = getConnection("players");
 			int responseCode = connection.getResponseCode();
 			if (responseCode == 200) {
 				String response = readAll(connection.getInputStream());
@@ -73,8 +126,12 @@ class NeptunesPride {
 						.serializeNulls()
 						.setPrettyPrinting()
 						.create();
-				AllPlayers temp = gson.fromJson(response, AllPlayers.class);
-				players = temp.getPlayers();
+				Type token = new TypeToken<HashMap<String, Player>>() {
+				}.getType();
+				/*AllPlayers temp = gson.fromJson(response, AllPlayers.class);
+				players = temp.getPlayers();*/
+				HashMap<String, Player> temp = gson.fromJson(response, token);
+				players = new TreeSet<>(temp.values());
 			}
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
