41f228bbb9251071c65eccff5d934bb554c21ec2
Macor303 <jackson.jonah@gmail.com>
2018-11-13 17:00:14 +1300

    Updates to endpoints/content-type

--- src/main/kotlin/macro/neptunes/Application.kt
+++ src/main/kotlin/macro/neptunes/Application.kt
@@ -4,7 +4,6 @@ import com.google.gson.GsonBuilder
 import org.apache.logging.log4j.LogManager
 import spark.kotlin.*
 
-
 /**
  * Created by Macro303 on 2018-Nov-12.
  */
@@ -25,35 +24,81 @@ object Application {
 		port(5001)
 
 		before {
-			if (request.requestMethod() == "HEAD" || request.requestMethod() == "GET")
-				LOGGER.info("${request.protocol()} ${request.requestMethod()} >> Endpoint: ${request.pathInfo()}, Content-Type: ${request.contentType()}, Agent: ${request.userAgent()}")
-			if (request.requestMethod() == "POST")
-				LOGGER.info("${request.protocol()} ${request.requestMethod()} >> Endpoint: ${request.pathInfo()}, Content-Type: ${request.contentType()}, Body: ${request.body().toJSON()}, Agent: ${request.userAgent()}")
+			when (request.requestMethod()) {
+				"HEAD" -> LOGGER.info("${request.protocol()} ${request.requestMethod()} >> Endpoint: ${request.pathInfo()}, Content-Type: ${request.contentType()}, Agent: ${request.userAgent()}")
+				"GET" -> LOGGER.info("${request.protocol()} ${request.requestMethod()} >> Endpoint: ${request.pathInfo()}, Content-Type: ${request.contentType()}, Agent: ${request.userAgent()}")
+				"POST" -> LOGGER.info("${request.protocol()} ${request.requestMethod()} >> Endpoint: ${request.pathInfo()}, Content-Type: ${request.contentType()}, Body: ${request.body().toJSON()}, Agent: ${request.userAgent()}")
+			}
 		}
 		finally {
 			LOGGER.info("${request.protocol()} ${response.status()} << Content-Type: ${response.type()}, Body: ${response.body()}")
 		}
 
-		get("/hello") {
-			type("application/json")
-			if (request.contentType() != "application/json")
-				contentException(request.contentType())
-			mapOf(Pair("Message", "Hello")).toJSON()
+		get("/") {
+			when {
+				request.contentType() == "text/plain" || request.contentType() == null -> {
+					type(contentType = "text/plain")
+					"Welcome to BIT 269's Neptune's Pride"
+				}
+				request.contentType() == "application/json" -> {
+					type(contentType = "application/json")
+					mapOf(Pair("Message", "Welcome to BIT 269's Neptune's Pride")).toJSON()
+				}
+				else -> contentTypeException(handler = this)
+			}
+		}
+
+		get("/players") {
+			when {
+				request.contentType() == "text/plain" || request.contentType() == null -> {
+					val playerName = request.queryParamOrDefault("name", null)
+					LOGGER.info("Player Name: $playerName")
+					type(contentType = "text/plain")
+					"Player Details"
+				}
+				request.contentType() == "application/json" -> {
+					val playerName = request.queryParamOrDefault("name", null)
+					LOGGER.info("Player Name: $playerName")
+					type(contentType = "application/json")
+					mapOf(Pair("Player", Pair("Name", playerName))).toJSON()
+				}
+				else -> contentTypeException(handler = this)
+			}
+		}
+
+		get("/refresh"){
+			when {
+				request.contentType() == "text/plain" || request.contentType() == null -> {
+					response.status(204)
+				}
+				request.contentType() == "application/json" -> {
+					response.status(204)
+				}
+				else -> contentTypeException(handler = this)
+			}
 		}
 
 		notFound {
-			type("application/json")
-			val message = mapOf(Pair("Message", "Unable to find the endpoint you are looking for: '${request.pathInfo()}'")).toJSON()
-			message
+			when {
+				request.contentType() == "text/plain" || request.contentType() == null -> {
+					type(contentType = "text/plain")
+					"Unable to find the endpoint you are looking for: '${request.pathInfo()}'"
+				}
+				else -> {
+					type(contentType = "application/json")
+					mapOf(Pair("Error", "Unable to find the endpoint you are looking for: '${request.pathInfo()}'")).toJSON()
+				}
+			}
 		}
 	}
 
-	fun Any?.toJSON(): String {
+	private fun Any?.toJSON(): String {
 		return GSON.toJson(this)
 	}
 
-	fun contentException(content: String) {
-		val message = mapOf(Pair("Message", "This endpoint doesn't support '$content' Content-Type")).toJSON()
-		halt(403, message)
+	private fun contentTypeException(handler: RouteHandler) {
+		handler.response.type("application/json")
+		val message = mapOf(Pair("Error", "This endpoint doesn't support '${handler.request.contentType()}' Content-Type")).toJSON()
+		halt(400, message)
 	}
 }
